676d3242a5ca04074308a6edd58fcea516cef5ad
Gregory Hellings <greg.hellings@gmail.com>
2021-05-07 14:26:48 -0500

    DRY the versioning of the code

--- get_sources.sh
+++ get_sources.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pkg="$(readlink -f "$(dirname "${0}")")"
-version=34
+version=$(cat DistroLauncher-Appx/FedoraWSL.appxmanifest | grep 'Name="FedoraWSL" Version=' | sed -E -e 's/^.*Version="([.0-9]*)".*$/\1/' | cut -d. -f1)
 
+sed -E -e "s/latest/${version}/" Containerfile.in > Containerfile
 podman build -t fedwsl:${version} "${pkg}"
 podman run --name fedwsl fedwsl:${version} /bin/bash
 podman export -o install.tar fedwsl
